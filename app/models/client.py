074621de8e2aed0c73988bab0b18761fc83646fd
Colin Ries <colin.ries@gmail.com>
2023-12-20 00:47:57 +0000

    match user model. More functional for raw SQL on server

--- app/models/client.py
+++ app/models/client.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, UUID
+from sqlalchemy import String, UUID, text
 from uuid import uuid4
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database.db import Base
@@ -7,8 +7,8 @@ from app.database.db import Base
 class Client(Base):
     __tablename__ = "clients"
 
-    id: Mapped[UUID] = mapped_column(UUID, name="id", primary_key=True, nullable=False, default=uuid4, index=True, quote=False)
-    firstName: Mapped[str] = mapped_column(String(40), name="firstName", nullable=False, index=True, quote=False)
-    lastName: Mapped[str] = mapped_column(String(40), name="lastName", nullable=False, index=True, quote=False)
-    emailAddress: Mapped[str] = mapped_column(String(255), name="emailAddress", nullable=False, quote=False)
+    id: Mapped[UUID] = mapped_column("id", UUID, primary_key=True, nullable=False, default=uuid4, server_default=text("uuid_generate_v4()"), index=True, quote=False)
+    firstName: Mapped[str] = mapped_column("firstName", String(40), nullable=False, index=True, quote=False)
+    lastName: Mapped[str] = mapped_column("lastName", String(40), nullable=False, index=True, quote=False)
+    emailAddress: Mapped[str] = mapped_column("emailAddress", String(255), nullable=False, quote=False)
 
