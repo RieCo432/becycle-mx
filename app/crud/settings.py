4028e81a29003ded74138025063374e6c8e52f56
Colin Ries <colin.ries@gmail.com>
2023-12-31 18:50:36 +0000

    moved maximum concurrent slots availability functions to settings crud

--- app/crud/settings.py
+++ app/crud/settings.py
@@ -1,4 +1,8 @@
-from datetime import date
+from copy import copy
+from datetime import date, datetime, time
+
+from dateutil.relativedelta import relativedelta
+from dateutil.rrule import rrule, DAILY
 from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -115,7 +119,67 @@ def get_opening_week_days(db: Session) -> list[int]:
         select(models.AppointmentGeneralSettings.openingDays)
     )
 
+
 def get_slot_duration(db: Session) -> int:
     return db.scalar(
         select(models.AppointmentGeneralSettings.slotDuration)
     )
+
+
+def get_open_days(db: Session) -> list[date]:
+    period_start = datetime.utcnow().date() + relativedelta(days=get_min_book_ahead(db=db))
+    period_end = datetime.utcnow().date() + relativedelta(days=get_max_book_ahead(db=db))
+
+    closed_dates = get_closed_dates_after_date(db=db, after_date=period_start)
+
+    opening_weekdays = get_opening_week_days(db=db)
+
+    open_days = [d.date() for d in rrule(DAILY, dtstart=period_start, until=period_end)
+                 if (d.weekday() in opening_weekdays)
+                 and d.date() not in closed_dates]
+
+    return open_days
+
+
+def get_maximum_concurrent_appointments_for_each_slot_of_day(db: Session) -> dict[time, int]:
+    appointment_concurrency_limits_by_time_after = {acl.afterTime: acl.maxConcurrent for acl in get_appointment_concurrency_limits(db=db)}
+    sorted_appointment_concurrency_limit_times = sorted(appointment_concurrency_limits_by_time_after.keys())
+
+    slot_duration = get_slot_duration(db=db)
+
+    maximum_concurrent_appointments_for_each_slot_of_day = {}
+    # get the first time that has a limit higher than 0
+    # this avoids iterating over all the 15-minute slots between midnight and the first slot with a non 0 limit
+    t = next(t for t in sorted_appointment_concurrency_limit_times if appointment_concurrency_limits_by_time_after[t] > 0)
+
+    # manually add a limit of 0 for 15 minutes before the first non-zero slot
+    # this has no functional role, but might add a bit of visual padding to the resulting table
+    maximum_concurrent_appointments_for_each_slot_of_day[
+        (datetime.combine(date(year=1, month=1, day=1), t) - relativedelta(minutes=slot_duration)).time()
+    ] = 0
+
+    active_concurrency_limit = appointment_concurrency_limits_by_time_after[t]
+    while t <= time(hour=23, minute=59):
+        maximum_concurrent_appointments_for_each_slot_of_day[t] = active_concurrency_limit
+
+        # if we have reached the last concurrency rule and the limit is 0, exit the loop
+        if t == sorted_appointment_concurrency_limit_times[-1] and active_concurrency_limit == 0:
+            break
+
+        # to add 15 minutes to a time object, we create a full datetime object using 01/01/01 as a date
+        # add 15 minutes relativedelta and then get only the time of the resulting datetime object
+        t = (datetime.combine(date(year=1, month=1, day=1), t) + relativedelta(minutes=slot_duration)).time()
+        active_concurrency_limit = appointment_concurrency_limits_by_time_after.get(t, active_concurrency_limit)
+
+    return maximum_concurrent_appointments_for_each_slot_of_day
+
+
+def get_maximum_concurrent_appointments_for_each_slot(db: Session) -> dict[date, dict[time, int]]:
+    maximum_concurrent_appointments_for_each_slot_of_day = get_maximum_concurrent_appointments_for_each_slot_of_day(db=db)
+
+    maximum_concurrent_appointments_for_each_slot = {
+        d: copy(maximum_concurrent_appointments_for_each_slot_of_day)
+        for d in get_open_days(db=db)
+    }
+
+    return maximum_concurrent_appointments_for_each_slot
