887f5d00d6c658d0438f85dbd054e24e6bdaae85
Colin Ries <colin.ries@gmail.com>
2024-03-04 13:29:10 +0000

    return proper error description

--- app/routers/users.py
+++ app/routers/users.py
@@ -27,9 +27,9 @@ async def get_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
     user = crud.authenticate_user(username=form_data.username, password_cleartext=form_data.password, db=db)
 
     if user is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect username or password")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"description": "Incorrect username or password"})
     if user.softDeleted:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="This user has been soft-deleted")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"description": "Use has been soft-deleted"})
 
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = auth.create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
