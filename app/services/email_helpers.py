34d18a7088577145f3fa69eb7b35d58d3f11abe3
Colin Ries <colin.ries@gmail.com>
2024-03-04 15:36:45 +0000

    PRODUCTION env variable determines if sending email or printing to console

--- app/services/email_helpers.py
+++ app/services/email_helpers.py
@@ -14,7 +14,7 @@ SMTP_PORT = os.environ['SMTP_PORT']
 SMTP_SERVER = os.environ['SMTP_SERVER']
 API_HOST_ADDRESS = os.environ['API_HOST_ADDRESS']
 API_HOST_PORT = os.environ['API_HOST_PORT']
-
+PRODUCTION = os.environ['PRODUCTION'] == "true"
 
 weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -28,7 +28,7 @@ def send_email(destination: str, subject: str, content: str) -> None:
 
     message.attach(MIMEText(content, "html"))
 
-    if os.environ["PRODUCTION"]:
+    if PRODUCTION:
 
         context = ssl.create_default_context()
 
