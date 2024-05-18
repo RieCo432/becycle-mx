67ed5a544df8f906a4413169674bb1ad430fe6f7
Colin Ries <colin.ries@gmail.com>
2024-05-18 15:46:27 +0100

    100% coverage on survey router and crud

--- app/crud/surveys.py
+++ app/crud/surveys.py
@@ -8,29 +8,19 @@ import app.schemas as schemas
 def create_pre_becycle_survey_entry(db: Session, survey_answers: schemas.PreBecycleSurvey) -> None:
     survey_entry = models.PreBecycleSurvey(**survey_answers.model_dump())
 
-    try:
-        db.add(survey_entry)
-        db.commit()
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"description": "Something went wrong"})
+    db.add(survey_entry)
+    db.commit()
 
 
 def create_peri_becycle_survey_entry(db: Session, survey_answers: schemas.PeriBecycleSurvey) -> None:
     survey_entry = models.PeriBecycleSurvey(**survey_answers.model_dump())
 
-    try:
-        db.add(survey_entry)
-        db.commit()
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"description": "Something went wrong"})
+    db.add(survey_entry)
+    db.commit()
 
 
 def create_post_becycle_survey_entry(db: Session, survey_answers: schemas.PostBecycleSurvey) -> None:
     survey_entry = models.PostBecycleSurvey(**survey_answers.model_dump())
 
-    try:
-        db.add(survey_entry)
-        db.commit()
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"description": "Something went wrong"})
+    db.add(survey_entry)
+    db.commit()
--- app/tests/routers/__init__.py
+++ app/tests/routers/__init__.py
@@ -1,3 +1,4 @@
 from .public import *
 from .user import *
 from .bike import *
+from .surveys import *
