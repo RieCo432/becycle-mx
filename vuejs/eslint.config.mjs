6aabcd90a22ee35209fb24df493daa73480df897
Colin Ries <colin.ries@gmail.com>
2024-12-16 15:24:48 +0000

    #53 added/updated some ESLint modules and customised some rules

---
+++ vuejs/eslint.config.mjs
@@ -0,0 +1,51 @@
+import vue from 'eslint-plugin-vue';
+import globals from 'globals';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [...compat.extends('google', 'plugin:vue/vue3-essential'), {
+  plugins: {
+    vue,
+  },
+
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+    },
+
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  rules: {
+    'linebreak-style': ['off', 'unix'],
+    'indent': ['error', 2],
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
+
+    'max-len': ['error', {
+      code: 140,
+    }],
+  },
+}, {
+  files: ['**/.eslintrc.{js,cjs}'],
+
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+
+    ecmaVersion: 5,
+    sourceType: 'commonjs',
+  },
+}];
