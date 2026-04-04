e459612b74d2c235fba3a0e8094ddcae62886518
Colin Ries <colin.ries@gmail.com>
2026-04-04 23:43:56 +0100

    correctly show the return accepting user on a contract

--- vuejs/src/views/contract/index.vue
+++ vuejs/src/views/contract/index.vue
@@ -38,9 +38,7 @@ export default {
       depositAssetAccounts: [],
       depositRevenueAccounts: [],
       activeUsers: [],
-      returnAcceptedByUser: {
-        username: 'null',
-      },
+      returnAcceptedByUser: {},
       loadingBike: true,
       loadingClient: true,
       loadingContract: true,
@@ -73,12 +71,15 @@ export default {
         Promise.all([
           requests.getUser(this.contract['workingUserId']),
           requests.getUser(this.contract['checkingUserId']),
+          ...(this.contract['returnAcceptingUserId'] ? [requests.getUser(this.contract['returnAcceptingUserId'])] : []),
         ]).then(([
           workingUserResponse,
           checkingUserResponse,
+          returnAcceptedByUserResponse,
         ]) => {
           this.workingUser = workingUserResponse.data;
           this.checkingUser = checkingUserResponse.data;
+          this.returnAcceptedByUser = returnAcceptedByUserResponse ? returnAcceptedByUserResponse.data : null;
           this.loadingContract = false;
         });
       });
