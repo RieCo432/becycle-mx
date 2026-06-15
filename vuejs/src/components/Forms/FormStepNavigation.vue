3771e3c68844c8b8868249c4ca5ed3c338246862
Colin Ries <colin.ries@gmail.com>
2026-06-15 15:11:10 +0100

    #403 start of a major refactor of the new contract component

---
+++ vuejs/src/components/Forms/FormStepNavigation.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+import Button from '@/components/Button/index.vue';
+import {defineProps} from 'vue';
+
+const props = defineProps({
+  goBack: {
+    type: Function,
+    required: true,
+  },
+  submit: {
+    type: Function,
+    required: true,
+  },
+  processingSubmit: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+</script>
+
+<template>
+  <div
+    class="mt-10 flex justify-between"
+  >
+    <Button
+      @click.prevent="goBack"
+      :disabled="processingSubmit"
+      text="Prev"
+      btnClass="btn-dark"
+    />
+    <Button
+      btnClass="btn-dark"
+      :disabled="processingSubmit"
+      text="Next"
+    />
+    <VueSpinner v-if="processingSubmit" size="20px" class="text-sky-500"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
