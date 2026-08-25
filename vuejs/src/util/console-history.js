eb272cd8381abbe83dd257f3f1be29ff2aeccd3b
Colin Ries <colin.ries@gmail.com>
2026-08-25 22:51:51 +0100

    formatting

--- vuejs/src/util/console-history.js
+++ vuejs/src/util/console-history.js
@@ -6,92 +6,105 @@
  * https://git.io/console
  */
 
-'use strict'
+'use strict';
 
-import Queue from "@/util/queue";
+import Queue from '@/util/queue';
 
 /* Allow only one instance of console-history.js */
 if (typeof console.history !== 'undefined') {
-    throw new Error('Only one instance of console-history.js can run at a time.')
+  throw new Error('Only one instance of console-history.js can run at a time.');
 }
 
 /* Store the original log functions. */
-console._log = console.log
-console._info = console.info
-console._warn = console.warn
-console._error = console.error
-console._debug = console.debug
+console._log = console.log;
+console._info = console.info;
+console._warn = console.warn;
+console._error = console.error;
+console._debug = console.debug;
 
 /* Declare our console history variable. */
-console.history = new Queue()
+console.history = new Queue();
 
 /* Redirect all calls to the collector. */
-console.log = function () { return console._intercept('log', arguments) }
-console.info = function () { return console._intercept('info', arguments) }
-console.warn = function () { return console._intercept('warn', arguments) }
-console.error = function () { return console._intercept('error', arguments) }
-console.debug = function () { return console._intercept('debug', arguments) }
+console.log = function() {
+  return console._intercept('log', arguments);
+};
+console.info = function() {
+  return console._intercept('info', arguments);
+};
+console.warn = function() {
+  return console._intercept('warn', arguments);
+};
+console.error = function() {
+  return console._intercept('error', arguments);
+};
+console.debug = function() {
+  return console._intercept('debug', arguments);
+};
 
 /* Give the developer the ability to intercept the message before letting console-history access it. */
-console._intercept = function (type, args) {
-    // Your own code can go here, but the preferred method is to override this
-    // function in your own script, and add the line below to the end or
-    // begin of your own 'console._intercept' function.
-    // REMEMBER: Use only underscore console commands inside _intercept!
-    console._collect(type, args)
-}
+console._intercept = function(type, args) {
+  // Your own code can go here, but the preferred method is to override this
+  // function in your own script, and add the line below to the end or
+  // begin of your own 'console._intercept' function.
+  // REMEMBER: Use only underscore console commands inside _intercept!
+  console._collect(type, args);
+};
 
 /* Define the main log catcher. */
-console._collect = function (type, args) {
-    // WARNING: When debugging this function, DO NOT call a modified console.log
-    // function, all hell will break loose.
-    // Instead use the original console._log functions.
+console._collect = function(type, args) {
+  // WARNING: When debugging this function, DO NOT call a modified console.log
+  // function, all hell will break loose.
+  // Instead use the original console._log functions.
 
-    // All the arguments passed to any function, even when not defined
-    // inside the function declaration, are stored and locally available in
-    // the variable called 'arguments'.
-    //
-    // The arguments of the original console.log functions are collected above,
-    // and passed to this function as a variable 'args', since 'arguments' is
-    // reserved for the current function.
+  // All the arguments passed to any function, even when not defined
+  // inside the function declaration, are stored and locally available in
+  // the variable called 'arguments'.
+  //
+  // The arguments of the original console.log functions are collected above,
+  // and passed to this function as a variable 'args', since 'arguments' is
+  // reserved for the current function.
 
-    // Collect the timestamp of the console log.
-    var time = new Date().toUTCString()
+  // Collect the timestamp of the console log.
+  const time = new Date().toUTCString();
 
-    // Make sure the 'type' parameter is set. If no type is set, we fall
-    // back to the default log type.
-    if (!type) type = 'log'
+  // Make sure the 'type' parameter is set. If no type is set, we fall
+  // back to the default log type.
+  if (!type) type = 'log';
 
-    // To ensure we behave like the original console log functions, we do not
-    // output anything if no arguments are provided.
-    if (!args || args.length === 0) return
+  // To ensure we behave like the original console log functions, we do not
+  // output anything if no arguments are provided.
+  if (!args || args.length === 0) return;
 
-    // Act normal, and just pass all original arguments to
-    // the original console function :)
-    console['_' + type].apply(console, args)
+  // Act normal, and just pass all original arguments to
+  // the original console function :)
+  console['_' + type].apply(console, args);
 
-    // Get stack trace information. By throwing an error, we get access to
-    // a stack trace. We then go up in the trace tree and filter out
-    // irrelevant information.
-    var stack = false
-    try { throw Error('') } catch (error) {
-        // The lines containing 'console-history.js' are not relevant to us.
-        var stackParts = error.stack.split('\n')
-        stack = []
-        for (var i = 0; i < stackParts.length; i++) {
-            if (stackParts[i].indexOf('console-history.js') > -1 ||
+  // Get stack trace information. By throwing an error, we get access to
+  // a stack trace. We then go up in the trace tree and filter out
+  // irrelevant information.
+  let stack = false;
+  try {
+    throw Error('');
+  } catch (error) {
+    // The lines containing 'console-history.js' are not relevant to us.
+    const stackParts = error.stack.split('\n');
+    stack = [];
+    for (let i = 0; i < stackParts.length; i++) {
+      if (stackParts[i].indexOf('console-history.js') > -1 ||
                 stackParts[i].indexOf('console-history.min.js') > -1 ||
                 stackParts[i] === 'Error') {
-                continue
-            }
-            stack.push(stackParts[i].trim())
-        }
+        continue;
+      }
+      stack.push(stackParts[i].trim());
     }
+  }
 
-    if(console.history.size() === 20)
-        console.history.dequeue()
+  if (console.history.size() === 20) {
+    console.history.dequeue();
+  }
 
-    // Add the log to our history.
-    console.history.enqueue({ type: type, timestamp: time, arguments: args, stack: stack })
-    localStorage.setItem('bugReportConsoleHistory', JSON.stringify(console.history))
-}
+  // Add the log to our history.
+  console.history.enqueue({type: type, timestamp: time, arguments: args, stack: stack});
+  localStorage.setItem('bugReportConsoleHistory', JSON.stringify(console.history));
+};
