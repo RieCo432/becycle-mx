82deba459b41de86b50273a3141c05359f17a186
Colin Ries <colin.ries@gmail.com>
2026-09-06 20:51:02 +0100

    #459 fix user leaderboard. Deposit information is useless

--- fastapi/app/crud/statistics.py
+++ fastapi/app/crud/statistics.py
@@ -21,10 +21,6 @@ def get_user_leaderboard(db: Session) -> list[schemas.UserLeaderboard]:
         contracts_done = len(user.workedContracts)
         contracts_checked = len(user.checkedContracts)
         contracts_returned = len(user.returnedContracts)
-        deposits_collected = len(user.depositCollectedContracts)
-        deposit_amount_collected = sum([contract.depositAmountCollected for contract in user.depositCollectedContracts])
-        deposits_returned = len(user.depositReturnedContracts)
-        deposit_amount_returned = sum([contract.depositAmountReturned for contract in user.depositReturnedContracts])
 
         leaderboard_entry = schemas.UserLeaderboard(
             id=user.id,
@@ -32,10 +28,6 @@ def get_user_leaderboard(db: Session) -> list[schemas.UserLeaderboard]:
             contractsDone=contracts_done,
             contractsChecked=contracts_checked,
             contractsReturned=contracts_returned,
-            depositsCollected=deposits_collected,
-            depositAmountCollected=deposit_amount_collected,
-            depositsReturned=deposits_returned,
-            depositAmountReturned=deposit_amount_returned
         )
 
         leaderboard.append(leaderboard_entry)
