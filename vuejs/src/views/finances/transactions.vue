ec77e6e3a0b053d290ed8ec7ec5941c7852a1dab
Colin Ries <colin.ries@gmail.com>
2026-09-12 23:00:06 +0100

    add pagination to the transaction table

--- vuejs/src/views/finances/transactions.vue
+++ vuejs/src/views/finances/transactions.vue
@@ -2,41 +2,56 @@
 import requests from '@/requests';
 import Card from '@/components/Card/index.vue';
 import TransactionsTable from '@/components/Tables/TransactionsTable.vue';
+import Pagination from '@/components/Pagination/index.vue';
 
 export default {
   name: 'transactions',
-  components: {TransactionsTable, Card},
+  components: {Pagination, TransactionsTable, Card},
   data() {
     return {
       transactions: [],
+      loading: true,
+      currentPage: 1,
+      perPage: 10,
     };
   },
   mounted() {
     this.fetchTransactions();
   },
   methods: {
+    transactionSorting(a, b) {
+      return new Date(b.createdOn) - new Date(a.createdOn);
+    },
     fetchTransactions() {
       requests.getFormattedTransactionHeaders().then((response) => {
-        this.transactions = response.data.map((transaction) => ({
-          id: transaction.id,
-          event: transaction.event,
-          createdOn: transaction.createdOn,
-          createdByUsername: transaction.createdByUsername,
-          postedOn: transaction.postedOn,
-          postedByUsername: transaction.postedByUsername,
-          credit: null,
-          debit: null,
-          accountName: transaction.accountName,
-          children: transaction.formattedTransactionLines.map((line) => ({
-            id: line.id,
-            createdOn: null,
-            postedOn: null,
-            accountName: line.accountName,
-            credit: line.credit,
-            debit: line.debit,
-          })),
-        }));
+        this.transactions = response.data
+          .map((transaction) => ({
+            id: transaction.id,
+            event: transaction.event,
+            createdOn: transaction.createdOn,
+            createdByUsername: transaction.createdByUsername,
+            postedOn: transaction.postedOn,
+            postedByUsername: transaction.postedByUsername,
+            credit: null,
+            debit: null,
+            accountName: transaction.accountName,
+            children: transaction.formattedTransactionLines.map((line) => ({
+              id: line.id,
+              createdOn: null,
+              postedOn: null,
+              accountName: line.accountName,
+              credit: line.credit,
+              debit: line.debit,
+            })),
+          }))
+          .sort(this.transactionSorting);
+        this.loading = false;
       });
+    },
+  },
+  computed: {
+    transactionsPaged() {
+      return this.transactions.slice((this.currentPage - 1) * this.perPage, this.currentPage * this.perPage);
     },
   },
 };
@@ -45,10 +60,17 @@ export default {
 <template>
   <div class="grid grid-cols-12 gap-5">
     <div class="col-span-12">
-      <Card title="Accounts Management" class="dark:text-slate-300 text-slate-700">
+      <Card title="Transaction History" class="dark:text-slate-300 text-slate-700">
         <div class="grid grid-cols-12">
           <div class="col-span-12">
-            <TransactionsTable :transactions="transactions"/>
+            <Pagination
+              :total="transactions.length"
+              :current="currentPage"
+              @page-changed="(page) => currentPage = page"
+              :per-page="perPage"
+              :pageRange="5"
+            />
+            <TransactionsTable :loading="loading" :transactions="transactionsPaged"/>
           </div>
         </div>
       </Card>
