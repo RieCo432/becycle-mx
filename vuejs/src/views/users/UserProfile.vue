fe1c0b008a9b0544541b1097f58039ee41c22193
Colin Ries <colin.ries@gmail.com>
2026-09-13 21:00:37 +0100

    #425 passing getters instead of data makes it more modular

--- vuejs/src/views/users/UserProfile.vue
+++ vuejs/src/views/users/UserProfile.vue
@@ -1,16 +1,35 @@
 <script setup>
 import UserPresentationCard from '@/components/Card/UserPresentationCard.vue';
-import {defineProps} from 'vue';
-import Card from '@/components/Card/index.vue';
-import TableSkeleton from '@/components/Skeleton/TableSkeleton.vue';
+import {defineProps, ref} from 'vue';
 import GroupsCard from '@/components/Card/GroupsCard.vue';
 import UserContractsCard from '@/components/Card/UserContractsCard.vue';
 import AccountsCard from '@/components/Card/AccountsCard.vue';
+import requests from '@/requests';
 
 const props = defineProps({
-  presentationCard: {
-    type: Object,
-    required: false,
+  getUserPresentationCard: {
+    type: Function,
+    required: true,
+  },
+
+  getUserAccounts: {
+    type: Function,
+    required: true,
+  },
+
+  getUserGroups: {
+    type: Function,
+    required: true,
+  },
+
+  getUserContracts: {
+    type: Function,
+    required: true,
+  },
+
+  getUser: {
+    type: Function,
+    required: true,
   },
   updateCardDetails: {
     type: Function,
@@ -20,53 +39,137 @@ const props = defineProps({
     type: Function,
     required: false,
   },
-  loadingPresentationCard: {
-    type: Boolean,
-    required: false,
-  },
-  userAccounts: {
-    type: Array,
-    required: false,
-  },
-  loadingUserAccounts: {
-    type: Boolean,
-    required: false,
-  },
-  userGroups: {
-    type: Array,
-    required: false,
-  },
-  loadingUserGroups: {
-    type: Boolean,
-    required: false,
-  },
-  groupAccounts: {
-    type: Array,
-    required: false,
-  },
-  loadingGroupAccounts: {
-    type: Boolean,
-    required: false,
-  },
-  contracts: {
-    type: Array,
-    required: false,
-  },
-  loadingContracts: {
-    type: Boolean,
-    required: false,
-  },
-  photoUrl: {
-    type: String,
-    required: false,
-  },
-  user: {
-    type: Object,
-    required: false,
-  },
 });
 
+const user = ref(null);
+const presentationCardDetails = ref(null);
+const loadingPresentationCard = ref(true);
 
+const photoUrl = ref('null');
+const loadingPhoto = ref(true);
+
+const loadingContracts = ref(true);
+const contracts = ref([]);
+
+const loadingAccounts = ref(true);
+const accounts = ref([]);
+
+const loadingGroups = ref(true);
+const groups = ref([]);
+
+const loadingGroupAccounts = ref(true);
+const groupAccounts = ref([]);
+
+
+function getImage() {
+  if (presentationCardDetails.value.id !== 'NOTSET') {
+    requests.getPresentationCardPhoto(presentationCardDetails.value.id).then((response) => {
+      const photoFile = new File([response.data], {type: presentationCardDetails.value.photoContentType});
+      photoUrl.value = window.URL.createObjectURL(photoFile);
+    })
+      .catch((error) => {
+        toast.error(error.response.data.detail.description, {timeout: 2000});
+      })
+      .finally(() => {
+        loadingPhoto.value = false;
+      });
+  }
+}
+
+
+props.getUser().then((response) => {
+  user.value = response.data;
+});
+
+function getPresentationCard() {
+  props.getUserPresentationCard().then((response) => {
+    presentationCardDetails.value = response.data;
+    getImage();
+  }).catch((error) => {
+    if (error.status !== 404) {
+    }
+    presentationCardDetails.value = {
+      name: 'NOT SET',
+      bio: 'NOT SET',
+      id: 'NOTSET',
+      photoContentType: 'image/jpeg',
+    };
+    photoUrl.value = null;
+    loadingPhoto.value = false;
+  }).finally(() => {
+    loadingPresentationCard.value = false;
+  });
+}
+
+getPresentationCard();
+
+
+props.getUserGroups().then((response) => {
+  groups.value = response.data;
+
+  const getGroupAccountsPromises = groups.value.map((group) => requests.getGroupAccounts(group.id));
+
+  Promise.all(getGroupAccountsPromises)
+    .then((responses) => {
+      responses.forEach((response) => {
+        response.data.forEach((account) => {
+          if (!groupAccounts.value.find((a) => a.id === account.id)) {
+            groupAccounts.value.push(account);
+          }
+        });
+      });
+    })
+    .catch((error) => {
+      toast.error(error.response.data.detail.description, {timeout: 2000});
+    })
+    .finally(() => {
+      loadingGroupAccounts.value = false;
+    });
+}).catch((error) => {
+  toast.error(error.response.data.detail.description, {timeout: 2000});
+}).finally(() => {
+  loadingGroups.value = false;
+});
+
+props.getUserAccounts().then((response) => {
+  accounts.value = response.data;
+}).catch((error) => {
+  toast.error(error.response.data.detail.description, {timeout: 2000});
+}).finally(() => {
+  loadingAccounts.value = false;
+});
+
+props.getUserContracts().then((response) => {
+  contracts.value = response.data;
+}).catch((error) => {
+  toast.error(error.response.data.detail.description, {timeout: 2000});
+}).finally(() => {
+  loadingContracts.value = false;
+});
+
+function updateCardDetailsWrapped(details) {
+  props.updateCardDetails(details).then((response) => {
+    presentationCardDetails.value = response.data;
+    getPresentationCard();
+    toast.success('Card Updated!', {timeout: 2000});
+  }).catch((error) => {
+    toast.error(error.response.data.detail.description, {timeout: 2000});
+  });
+}
+
+function deleteCardWrapped() {
+  props.deleteCard().then(() => {
+    presentationCardDetails.value = {
+      name: 'NOT SET',
+      bio: 'NOT SET',
+      id: 'NOTSET',
+      photoContentType: 'image/jpeg',
+    };
+    photoUrl.value = null;
+  }).catch((error) => {
+    toast.error(error.response.data.detail.description, {timeout: 2000});
+  });
+}
 
 </script>
 
@@ -75,18 +178,18 @@ const props = defineProps({
     <div class="col-span-6">
       <UserPresentationCard
         :loading="loadingPresentationCard"
-        :presentation-card-details="presentationCard"
+        :presentation-card-details="presentationCardDetails"
         editable
-        :update-item-details="updateCardDetails"
-        :delete-card="deleteCard"
+        :update-item-details="updateCardDetailsWrapped"
+        :delete-card="deleteCardWrapped"
         :photo-url="photoUrl"
       />
     </div>
     <div class="col-span-6">
       <AccountsCard
         title="User Accounts"
-        :accounts="userAccounts"
-        :loading="loadingUserAccounts"
+        :accounts="accounts"
+        :loading="loadingAccounts"
       />
     </div>
     <div class="col-span-12">
@@ -98,8 +201,8 @@ const props = defineProps({
     </div>
     <div class="col-span-6">
       <GroupsCard
-        :loading="loadingUserGroups"
-        :groups="userGroups"
+        :loading="loadingGroups"
+        :groups="groups"
       />
     </div>
     <div class="col-span-6">
--- vuejs/src/views/users/me.vue
+++ vuejs/src/views/users/me.vue
@@ -1,154 +1,18 @@
 <script setup>
 import requests from '@/requests';
-import {useToast} from 'vue-toastification';
-import {ref} from 'vue';
 import UserProfile from '@/views/users/UserProfile.vue';
-
-const toast = useToast();
-
-const user = ref(null);
-const presentationCardDetails = ref(null);
-const loadingPresentationCard = ref(true);
-
-const photoUrl = ref('null');
-const loadingPhoto = ref(true);
-
-const loadingMyContracts = ref(true);
-const myContracts = ref([]);
-
-const loadingMyAccounts = ref(true);
-const myAccounts = ref([]);
-
-const loadingMyGroups = ref(true);
-const myGroups = ref([]);
-
-const loadingMyGroupAccounts = ref(true);
-const myGroupAccounts = ref([]);
-
-function updateMyCardDetails(name, bio, photo) {
-  requests.postMyPresentationCardDetails(name, bio, photo).then((response) => {
-    presentationCardDetails.value = response.data;
-    toast.success('Card Updated!', {timeout: 2000});
-    getPresentationCard();
-  }).catch((error) => {
-    toast.error(error.response.data.detail.description, {timeout: 2000});
-  });
-}
-
-function getImage() {
-  if (presentationCardDetails.value.id !== 'NOTSET') {
-    requests.getPresentationCardPhoto(presentationCardDetails.value.id).then((response) => {
-      const photoFile = new File([response.data], {type: presentationCardDetails.value.photoContentType});
-      photoUrl.value = window.URL.createObjectURL(photoFile);
-    })
-      .catch((error) => {
-        toast.error(error.response.data.detail.description, {timeout: 2000});
-      })
-      .finally(() => {
-        loadingPhoto.value = false;
-      });
-  }
-}
-
-function deleteMyCard() {
-  requests.deleteMyPresentationCard().then(() => {
-    presentationCardDetails.value = {
-      name: 'NOT SET',
-      bio: 'NOT SET',
-      id: 'NOTSET',
-      photoContentType: 'image/jpeg',
-    };
-    photoUrl.value = null;
-  }).catch((error) => {
-    toast.error(error.response.data.detail.description, {timeout: 2000});
-  });
-}
-requests.getUserMe().then((response) => {
-  user.value = response.data;
-});
-function getPresentationCard() {
-  requests.getMyPresentationCard().then((response) => {
-    presentationCardDetails.value = response.data;
-    getImage();
-  }).catch((error) => {
-    if (error.status !== 404) {
-    }
-    presentationCardDetails.value = {
-      name: 'NOT SET',
-      bio: 'NOT SET',
-      id: 'NOTSET',
-      photoContentType: 'image/jpeg',
-    };
-    photoUrl.value = null;
-    loadingPhoto.value = false;
-  }).finally(() => {
-    loadingPresentationCard.value = false;
-  });
-}
-
-getPresentationCard();
-
-requests.getMyGroups().then((response) => {
-  myGroups.value = response.data;
-
-  const getGroupAccountsPromises = myGroups.value.map((group) => requests.getGroupAccounts(group.id));
-
-  Promise.all(getGroupAccountsPromises)
-    .then((responses) => {
-      responses.forEach((response) => {
-        response.data.forEach((account) => {
-          if (!myGroupAccounts.value.find((a) => a.id === account.id)) {
-            myGroupAccounts.value.push(account);
-          }
-        });
-      });
-    })
-    .catch((error) => {
-      toast.error(error.response.data.detail.description, {timeout: 2000});
-    })
-    .finally(() => {
-      loadingMyGroupAccounts.value = false;
-    });
-}).catch((error) => {
-  toast.error(error.response.data.detail.description, {timeout: 2000});
-}).finally(() => {
-  loadingMyGroups.value = false;
-});
-
-requests.getMyAccounts().then((response) => {
-  myAccounts.value = response.data;
-}).catch((error) => {
-  toast.error(error.response.data.detail.description, {timeout: 2000});
-}).finally(() => {
-  loadingMyAccounts.value = false;
-});
-
-requests.getUserMyContracts().then((response) => {
-  myContracts.value = response.data;
-}).catch((error) => {
-  toast.error(error.response.data.detail.description, {timeout: 2000});
-}).finally(() => {
-  loadingMyContracts.value = false;
-});
 
 </script>
 
 <template>
   <UserProfile
-    :loading-presentation-card="loadingPresentationCard || loadingPhoto"
-    :delete-card="deleteMyCard"
-    :update-card-details="updateMyCardDetails"
-    :presentation-card="presentationCardDetails"
-    :photo-url="photoUrl"
-    :user-accounts="myAccounts"
-    :loading-user-accounts="loadingMyAccounts"
-    :user-groups="myGroups"
-    :loading-user-groups="loadingMyGroups"
-    :group-accounts="myGroupAccounts"
-    :loading-group-accounts="loadingMyGroupAccounts"
-    :contracts="myContracts"
-    :loading-contracts="loadingMyContracts"
-    :user="user"
+    :get-user="requests.getUserMe"
+    :get-user-accounts="requests.getMyAccounts"
+    :get-user-contracts="requests.getUserMyContracts"
+    :get-user-groups="requests.getMyGroups"
+    :get-user-presentation-card="requests.getMyPresentationCard"
+    :delete-card="requests.deleteMyPresentationCard"
+    :update-card-details="requests.postMyPresentationCardDetails"
   />
 </template>
 
